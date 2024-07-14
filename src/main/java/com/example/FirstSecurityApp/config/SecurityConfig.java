d32d67865827d8a266fe9e8c7a7cbdec9c603783
Sasha <laplata228@gmail.com>
2024-07-14 13:39:02 +0300

    add login page

--- src/main/java/com/example/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/com/example/FirstSecurityApp/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.FirstSecurityApp.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -17,8 +18,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public SecurityConfig(PersonDetailsService personDetailsService) {
         this.personDetailsService = personDetailsService;
     }
-
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        //конфігурація самого спрінг секур.
+        //в тому числі конфігурація авторизації
+        http.csrf().disable() //відключення захисту
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error");
+    }
     //Настройки аутентифікації
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService); // TODO
     }
